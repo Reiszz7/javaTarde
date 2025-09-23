9913ff5dfe636b7aa04e8259d3322af797ef5fea
Fuuzzyy <0001075915@senaimgaluno.com.br>
2025-09-23 16:21:23 -0300

    Initial commit

---
+++ src/main/java/Exemplos/concatenar.java
@@ -0,0 +1,8 @@
+package Exemplos;
+
+public class concatenar {
+    public static void main(String[] args) {
+        int calca=600;
+        System.out.println("Uma calça pra uma jovem de 16 anos é mais de "+calca+" reais.");
+    }
+}
