58ca9da4ebc59c38851e8801554ff71d288af3a4
Caio Zico <caiozico08@gmail.com>
2025-09-26 18:13:14 -0300

    Adicionado os exercicios de 1 a 6, usando scanner no java

---
+++ src/main/java/Exemplos/entradaDeDados.java
@@ -0,0 +1,38 @@
+package Exemplos;
+
+import java.util.Scanner; // Importar o objeto scanner
+
+public class  entradaDeDados {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // Atribui a variável "sc" a leitura do teclado
+
+        String v1; // Declara v1 como string
+        int v2; // Declara v2 como inteiro
+        double v3; // Declara v3 como real
+        char v4; // Declara v4 como char (variável de valor = 1 caractere)
+
+        System.out.println("Digite a primeira String");
+        v1= sc.nextLine(); // Lê v1 como string
+
+        System.out.println("Digite um número inteiro");
+        v2= sc.nextInt(); // Lê v2 como inteiro
+
+        System.out.println("Digite um número real");
+        v3= sc.nextDouble(); // Lê v3 como real
+
+        System.out.println("Digite um caractere");
+        v4= sc.next().charAt(0); /* Lê v4 como char
+        .charAt(0) lê apenas o primeiro caractere digitado e ignora os outros, o número entre parênteses diz qual
+        caractere é lido (começando do 0) */
+
+        System.out.println("Dados digitados: ");
+        System.out.println(v1);
+        System.out.println(v2);
+        System.out.println(v3);
+        System.out.println(v4);
+        // Printa as respectivas variáveis
+
+        sc.close(); // Fecha  o objeto Scanner da variável sc
+
+    }
+}
--- src/main/java/Exemplos/helloWorld.java
+++
@@ -1,7 +0,0 @@
-package Exemplos;
-
-public class helloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
